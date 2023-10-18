541cca4204ffe02b241570e1a618558d0447f583
Shahreyar Tonmoy <shahreyartonmoy001@gmail.com>
2023-10-18 22:52:38 +0600

    get & post function done

--- index.js
+++ index.js
@@ -17,6 +17,74 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 console.log(uri);
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        const productCollaction = client.db("Adidas").collection('product')
+
+        app.get('/product',async(req,res) =>{
+            const cursor = productCollaction.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        app.get('/product/:id',async(req,res) =>{
+            const id = req.params.id
+            const query ={_id: new ObjectId(id) } 
+            const result = await productCollaction.findOne(query)
+            res.send(result)
+
+        })
+
+
+        app.post('/product', async(req,res)=>{
+            const addProduct =req.body
+            console.log(addProduct);
+            const result = await productCollaction.insertOne(addProduct)
+            res.send(result)
+        })
+
+        app.put('/product/:id',async(req,res) =>{
+            const id =req.params.id
+            const filter = {_id : new ObjectId(id) }
+            const options = { upsert : true}
+            const UpdateProduct = req.body
+            const Update = {
+                $set : {
+                    name: UpdateProduct.name,
+                     brand: UpdateProduct.brand,
+                      type: UpdateProduct.type,
+                       price: UpdateProduct.price,
+                        shortDescription: UpdateProduct.shortDescription,
+                         rating: UpdateProduct.rating,
+                          photoURL: UpdateProduct.photoURL
+                }
+            }
+            const result = await productCollaction.updateOne(filter, Update,options)
+            res.send(result)
+        })
+
+
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 
