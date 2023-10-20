dc070918af90b12dbbb3c13499439475c2c36cb0
Shahreyar Tonmoy <shahreyartonmoy001@gmail.com>
2023-10-20 15:06:07 +0600

    backend data fix

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.use(express.json())
 
 
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g0ipecx.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri);
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g0ipecx.mongodb.net/?retryWrites=true&w=majority`;
+
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -35,56 +35,61 @@ async function run() {
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
         const productCollaction = client.db("Adidas").collection('product')
-        const MyCartCollaction = client.db("Adidas").collection('MyCart')
+        const MyCartCollaction = client.db("Adidas").collection('mycart')
 
         app.get('/product', async (req, res) => {
             const cursor = productCollaction.find()
             const result = await cursor.toArray()
             res.send(result)
         })
-        app.get('/mycart', async (req, res) => {
-            const cursor = MyCartCollaction.find()
-            const result = await cursor.toArray()
-            res.send(result)
-        })
-        app.get('/product/:id', async (req, res) => {
+        
+        app.get('/product/:id' , async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const result = await productCollaction.findOne(query)
             res.send(result)
 
         })
-        app.get('/mycart/:id', async (req, res) => {
+        app.get('/mycart' , async (req, res) => {
+            const cursor = MyCartCollaction.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        app.post('/mycart' , async (req, res) => {
+            const addProduct = req.body
+            console.log(addProduct);
+            const result = await MyCartCollaction.insertOne(addProduct)
+            res.send(result)
+        })
+        app.get('/mycart/:id' , async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const result = await MyCartCollaction.findOne(query)
             res.send(result)
 
         })
-        app.delete('/mycart/:id',async (req, res)=>{
+        
+        
+        
+        app.delete('/mycart/:id' , async (req, res)=>{
             const id =req.params.id
             const query ={ _id: new ObjectId(id) }
-            const result =await MyCartCollaction.deleteOne(query)
+            const result = await MyCartCollaction.deleteOne(query)
             res.send(result)
         })
 
         // post opciton
-        app.post('/product', async (req, res) => {
+        app.post('/product' , async (req, res) => {
             const addProduct = req.body
             console.log(addProduct);
             const result = await productCollaction.insertOne(addProduct)
             res.send(result)
         })
-        app.post('/mycart', async (req, res) => {
-            const addProduct = req.body
-            console.log(addProduct);
-            const result = await MyCartCollaction.insertOne(addProduct)
-            res.send(result)
-        })
+        
 
         // update opction
 
-        app.put('/product/:id', async (req, res) => {
+        app.put('/product/:id' , async (req, res) => {
             const id = req.params.id
             const filter = { _id: new ObjectId(id) }
             const options = { upsert: true }
@@ -118,9 +123,9 @@ run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
-    res.send("coffe shop server is running")
+    res.send("Fashion store server is running")
 })
 
 app.listen(port, () => {
-    console.log(`coffe shop server is running on port : ${port}`);
+    console.log(`Fashion store server is running on port : ${port}`);
 })
